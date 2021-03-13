277455dfcb9d93e8f3b9d4bf8ee95ae70c90d077
Naughty <dorminsama@gmail.com>
2021-03-13 16:47:19 -0300

    Lazy loading routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,10 @@
-import { Component } from 'react';
+import { Suspense, Component, lazy } from 'react';
 import { BrowserRouter, HashRouter, Switch, Route } from 'react-router-dom';
 
 import MainScreen from './MainScreen';
 import MakeScreen from './MakeScreen';
-import NoteScreen from './NoteScreen';
+// import NoteScreen from './NoteScreen';
+const NoteScreen = lazy(() => import('./NoteScreen'));
 
 const Router: typeof Component = location.origin === 'file://'
     ? HashRouter
@@ -11,12 +12,14 @@ const Router: typeof Component = location.origin === 'file://'
 
 export default function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/make" component={MakeScreen} />
-                <Route path="/note/:id" component={NoteScreen} />
-                <Route path="/" component={MainScreen} />
-            </Switch>
-        </Router>
+        <Suspense fallback={<div></div>}>
+            <Router>
+                <Switch>
+                    <Route path="/make" component={MakeScreen} />
+                    <Route path="/note/:id" component={NoteScreen} />
+                    <Route path="/" component={MainScreen} />
+                </Switch>
+            </Router>
+        </Suspense>
     );
 }
